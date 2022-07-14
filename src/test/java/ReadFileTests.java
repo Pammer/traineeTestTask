2845620ef1ff63e50820115a015caae0d521e548
Markvaman <108291807+Markvaman@users.noreply.github.com>
2022-07-14 12:40:33 +0500

    Update ReadFileTests.java

--- src/test/java/ReadFileTests.java
+++ src/test/java/ReadFileTests.java
@@ -5,5 +5,12 @@ public class ReadFileTests {
     @Test
     public void secondLineShouldHabAlloha(){
       //write here second task
+      //read file and print text  
+    File file = new File("ReadFileTests.txt");
+    Scanner sc = new Scanner(file);
+
+    while(sc.hasNext()) {
+	System.out.println(sc.next());
     }
+  }
 }
