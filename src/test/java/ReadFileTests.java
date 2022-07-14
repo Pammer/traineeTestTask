e29f0048d50da8fabbe26c3a1281e1cdc5137e4b
Elena Balyberdina <elenaivanova854@gmail.com>
2022-07-14 16:09:27 +0300

    task_2

--- src/test/java/ReadFileTests.java
+++ src/test/java/ReadFileTests.java
@@ -1,9 +1,24 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ReadFileTests {
 
     @Test
-    public void secondLineShouldHabAlloha(){
-      //write here second task
+    public void secondLineShouldHabAlloha() {
+        //write here second task
+
+        String str;
+        try {
+            str = Files.readAllLines(Paths.get("src/test/resources/ReadFileTests.txt")).get(1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Assertions.assertTrue(str.contains("аллоха"));
+
     }
 }
